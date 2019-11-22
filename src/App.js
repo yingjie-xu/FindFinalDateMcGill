e2793d8a08de5383effe867cf2d2a117fd0741ee
yingjiex <xuyingjie619@gmail.com>
2019-11-21 19:57:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import data from "./new1.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      value: "",
+      course: false,
+      id: undefined
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+  componentDidMount() {
+    document.title = "Find Exam Date McGill";
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+    for (var k in data["COURSE NUMBER"]) {
+      if (data["COURSE NUMBER"][k] === event.target.value.toUpperCase()) {
+        this.setState({
+          isCourse: true,
+          id: k
+        })
+        break
+      } else {
+        this.setState({isCourse: false})
+      }
+    }
+  }
+
+  render() {
+    let display = undefined
+    if (this.state.isCourse) {
+      display = "The Final Exam of "
+      display += data["COURSE NUMBER"][this.state.id]
+      display += " will be held on "
+      display += data["EXAM DATE"][this.state.id]
+      display += " at "
+      display += data["TIME"][this.state.id]
+      display += ". "
+      if (data["NOTE"][this.state.id]) {
+        display += "Note: "
+        display += data["NOTE"][this.state.id]
+      }
+    } else {
+      display = "Invalid course! Please try again with the format \"XXXX(space)XXX\"!"
+    }
+    return (
+      <div className="App">
+        <div className="bg"></div>
+        <form>
+          <h1>Find your final exam date (McGill Univeristy)</h1>
+          <h2>Enter Course Number: </h2><input type="text" value={this.state.value} onChange={this.handleChange}></input>
+        </form>
+        <h2>{display}</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Follow <a href="https://www.mcgill.ca/exams/">this </a>link to get precise info. 
+          Follow <a href="https://www.mcgill.ca/exams/buildings">this </a>link to get exam location.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
+  
 }
 
 export default App;
