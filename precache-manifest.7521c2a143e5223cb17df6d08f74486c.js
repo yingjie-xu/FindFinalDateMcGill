2f2a52a6bccb074207903bcf73a072c9671cb6f1
yingjiex <xuyingjie619@gmail.com>
2019-11-21 19:58:29 -0500

    Updates

---
+++ precache-manifest.7521c2a143e5223cb17df6d08f74486c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd7e8875a243bcf867b52d2616f0957",
+    "url": "/FindFinalDateMcGill/index.html"
+  },
+  {
+    "revision": "bb1dd5a9e39843e5a78c",
+    "url": "/FindFinalDateMcGill/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4793b9f3c62029939302",
+    "url": "/FindFinalDateMcGill/static/js/2.58176741.chunk.js"
+  },
+  {
+    "revision": "bb1dd5a9e39843e5a78c",
+    "url": "/FindFinalDateMcGill/static/js/main.20ea1ce1.chunk.js"
+  },
+  {
+    "revision": "f84036754471e5ccac10",
+    "url": "/FindFinalDateMcGill/static/js/runtime-main.fe858fa1.js"
+  }
+]);
