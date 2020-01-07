c5c11e89563df779f006ec9210f988bce251e482
Jiahao <jahowchen@outlook.com>
2020-01-06 20:34:32 -0500

    Added two Components

---
+++ src/ExamCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Card, Col} from "antd";
+// <Row gutter={[24, 24]}>
+//TODO: make card content prettier, add icons or grids
+class ExamCard extends Component{
+    render(){
+    const {subj,title, section,date, time, building, room, row, from, to} = this.props;
+        return(
+        <Col md={12} xs={24}>
+            <Card title={`${subj} ${title} ${section}`} bordered={false}>
+                {date} {time} at {building} {room}
+                Row {row} From {from} To {to}
+            </Card>
+            </Col>
+        )
+    }
+}
+
+class CardGroup extends Component{
+
+}
