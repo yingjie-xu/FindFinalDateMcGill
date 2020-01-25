c285770a148af58f07d38b9df723819087de8f90
Yingjie Xu <xuyingjie619@gmail.com>
2020-01-25 11:36:13 -0500

    finished

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -13,6 +13,20 @@ class Cards extends Component{
       console.log(course_list)
       return (
         <div>
+          {course_list.length === 0 ? 
+            <div>
+              <h2>Instructions: </h2>
+              <h3>
+                <p>Start by click the "add course" button
+                  <br/>Type in your courses (e.g. comp 202)
+                  <br/>Click the "submit" button
+                  <br/>View your final schedule
+                  <br/>Note: Invalid courses will be ignored
+                </p>
+              </h3>
+            </div> 
+            : <div></div>
+          }
           {course_list.map(course => <ExamCard course={course} key={course['COURSE NUMBER'] + course["SECTION"] + course["ROW"]}/>)}
           {/* <ExportToCalendar exams={course_list}/> */}
         </div>
