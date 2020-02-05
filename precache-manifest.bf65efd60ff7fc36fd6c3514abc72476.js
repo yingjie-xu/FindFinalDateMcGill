5e084d0efd90ee04e35eb9edf4fda0a3a5fe62f8
Jiahao <jahowchen@outlook.com>
2020-02-05 16:09:32 -0500

    Updates

--- precache-manifest.bf65efd60ff7fc36fd6c3514abc72476.js
+++ precache-manifest.bf65efd60ff7fc36fd6c3514abc72476.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bbbfd46cf04750aa4b9e7821def4d8f",
+    "revision": "ad3774dd230b0034a85f201c1264a8f8",
     "url": "/FindFinalDateMcGill/index.html"
   },
   {
-    "revision": "c027cc8b573b4c0ece3e",
-    "url": "/FindFinalDateMcGill/static/css/2.83195336.chunk.css"
+    "revision": "d8ebbb1ce025ea8ba6cb",
+    "url": "/FindFinalDateMcGill/static/css/2.e6ecbbd3.chunk.css"
   },
   {
-    "revision": "bed7e8de5feec484bbee",
+    "revision": "8e23c29134220b05c76d",
     "url": "/FindFinalDateMcGill/static/css/main.5bec0469.chunk.css"
   },
   {
-    "revision": "c027cc8b573b4c0ece3e",
-    "url": "/FindFinalDateMcGill/static/js/2.af1c7c8d.chunk.js"
+    "revision": "d8ebbb1ce025ea8ba6cb",
+    "url": "/FindFinalDateMcGill/static/js/2.2851a20f.chunk.js"
   },
   {
-    "revision": "bed7e8de5feec484bbee",
-    "url": "/FindFinalDateMcGill/static/js/main.5bbd9b87.chunk.js"
+    "revision": "8e23c29134220b05c76d",
+    "url": "/FindFinalDateMcGill/static/js/main.deda4de6.chunk.js"
   },
   {
     "revision": "2d3e72d2d3a44b9acb4e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FindFinalDateMcGill/precache-manifest.759302c4e30b1b3397f4cd3001b49521.js"
+  "/FindFinalDateMcGill/precache-manifest.bf65efd60ff7fc36fd6c3514abc72476.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FindFinalDateMcGill/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
